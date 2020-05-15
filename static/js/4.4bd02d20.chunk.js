c28fabd78d4785cc6586d63995272fc0c50f8326
horitks <horitks@users.noreply.github.com>
2020-05-15 11:51:53 +0000

    deploy: 4512f5de55d65814b1be85dae02f7ad03d58f35a

---
+++ static/js/4.4bd02d20.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpshoppingTools=this.webpackJsonpshoppingTools||[]).push([[4],{180:function(n,t,o){"use strict";o.r(t),o.d(t,"startStatusTap",(function(){return i}));var e=o(15),i=function(){var n=window;n.addEventListener("statusTap",(function(){Object(e.h)((function(){var t=n.innerWidth,o=n.innerHeight,i=document.elementFromPoint(t/2,o/2);if(i){var s=i.closest("ion-content");s&&s.componentOnReady().then((function(){Object(e.n)((function(){return s.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=4.4bd02d20.chunk.js.map
